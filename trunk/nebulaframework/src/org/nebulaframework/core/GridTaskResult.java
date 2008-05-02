38f5dbb3da60bba1485adecb054714317d412e5f
yohanliyanage <yohanliyanage@12ddced4-fe4b-0410-8e1a-cb0bd439ce3d>
2008-05-02 17:20:02 +0000

    Started Coding Phase.
    Worked on package org.nebulaframework.core and wrote new classes for Job -> Task Split and related operations.

---
+++ trunk/nebulaframework/src/org/nebulaframework/core/GridTaskResult.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2008 Yohan Liyanage. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package org.nebulaframework.core;
+
+import java.io.Serializable;
+
+/**
+ * Wraps a result of {@link GridTask} execution with meta data.
+ * @author Yohan Liyanage
+ *
+ * @param <T> Type of Result of execution
+ */
+public interface GridTaskResult<T> extends Serializable {
+	
+	/**
+	 * Returns the exception thrown at execution, if any
+	 * @return Exception thrown, or <tt>null</tt>
+	 */
+	public Exception getException();
+	
+	/**
+	 * Returns the result of execution, if completed
+	 * @return Result of execution, or IllegalStateException if execution was failed
+	 */
+	public T getResult() throws IllegalStateException;
+	
+	/**
+	 * Returns true if the Task has completed successfully without exceptions.
+	 * @return true if Task completed
+	 */
+	public boolean isComplete();
+}
